b1a98690c341d1d32bf34bf759c7998ffd8f243b
THREEhundo <samuelbaik87@gmail.com>
2020-04-10 22:06:55 -0400

    Outline of the steps added.

---
+++ odin_on_rails/etch-a-sketch/js/script.js
@@ -0,0 +1,12 @@
+/*
+1. Create a 16 x 16 grid using JS
+2. Use a container
+3. Create a grid layout with CSS grid or flexbox
+4. Add a hover effect to the squares that changes the color.
+5. Add a button to the top of the screen to clear current grid.
+  i. It should prompt the user to select how many sqaures should be generated.
+  ii. Grid space should be static.
+  iii. Check out prompts.
+  iv. You should be able to populate 64 squares without changing the total pixel count.
+6. Have the hover effect select a random color. Use opacity to increment value by 10% each time.
+*/
