60c225fbf15148965ef2bda4f0af286c73dd95aa
THREEhundo <samuelbaik87@gmail.com>
2020-04-27 18:36:40 -0400

    fixed function for creating squares.

--- odin_on_rails/etch-a-sketch/js/script.js
+++ odin_on_rails/etch-a-sketch/js/script.js
@@ -24,16 +24,41 @@ board.className = 'board';
 board.style.backgroundImage = "url('images/etch-a-sketch.png')";
 document.body.appendChild(board);
 
-// inner board 16 x 16 grid
+// inner board
 const innerBoard = document.createElement('div');
 innerBoard.className = 'innerBoard';
+
+// function to create boxes for the etch-a-sketch and append them to the innerBoard
+function innerBoardSquares(x, y) {
+  // if there are divs clear the divs before proceeding
+  if (innerBoard.innerHTML) {
+    innerBoard.innerHTML = "";
+  }
+  // x & y have to be less than 9
+  if (x < 9 && y < 9) {
+    for (var i = 0; i < x * y; i++) {
+      // create x amount of divs
+      let newDiv = document.createElement('div');
+      // create class for all divs
+      newDiv.classList.add('square');
+      // create hover effect for each div
+
+      // edit grid template column and row
+      // append them to innerBoard
+      innerBoard.appendChild(newDiv);
+      console.log(i);
+    }
+  }
+  innerBoard.style.cssText = `grid-template-columns: repeat(${x}, minmax(50px, 1fr)); grid-template-rows:repeat(${y}, minmax(50px, 1fr));`
+  // grid-template-columns: repeat(4, minmax(50px, 1fr));
+  // grid-template-rows:repeat(4, minmax(50px, 1fr));
+}
+
+// 16 x 16 div grid is the default
+innerBoardSquares(3, 4);
+
+
+
 board.appendChild(innerBoard);
 
 // text field and button to customize grid
-
-
-
-// function to create boxes for the etch-a-sketch and append them to the innerBoard
-function innerBoardSquares() {
-
-}
