dfb555da83ee05cc4d0e56606f5ebf8f0d13e65d
THREEhundo <samuelbaik87@gmail.com>
2020-06-01 23:53:26 -0400

    palindromes start.

--- odin_on_rails/javascript-exercises-master/palindromes/palindromes.js
+++ odin_on_rails/javascript-exercises-master/palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+  let arr = [...word];
+  arr.reverse().join('');
+  return arr;
 }
-
+// letters are the same backwards and forwards
+// ommit spaces and punctuation
 module.exports = palindromes
