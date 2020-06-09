d90d5312b67b21e5b2da32b5bb53365c1c4382c4
THREEhundo <samuelbaik87@gmail.com>
2020-06-08 23:58:33 -0400

    wrote out the parameters for the for loop to get the fibonacci value

--- odin_on_rails/javascript-exercises-master/fibonacci/fibonacci.js
+++ odin_on_rails/javascript-exercises-master/fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+  //create a fibonacci array that has x amount of values
+  const fib = [];
+  // change the first value to the second value
+  // change the second value to the previous first value
+  // change the change the sum to first and second value
+  let sum = 0;
+  let first = 1;
+  let second = 1;
+  for (var i = 0; i < x; i++) {
+    if (i === 0) {
+      fib.push(first);
+    } else {
+      //console.log(nextDigit)
+      fib.push(
+        sum += firstDigit
+      )
+      firstDigit = sum
+      console.log(firstDigit)
+    }
+  }
 
+  return fib;
 }
 
 module.exports = fibonacci
