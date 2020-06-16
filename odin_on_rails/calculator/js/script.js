3fddb1d6c7cb32c73e03dea7c650483ee4750330
THREEhundo <samuelbaik87@gmail.com>
2020-06-16 19:26:58 -0400

    Numbers shown on View Span Backspace button implemented Clear button implemented

--- odin_on_rails/calculator/js/script.js
+++ odin_on_rails/calculator/js/script.js
@@ -1,3 +1,10 @@
+const buttons = document.querySelectorAll('button');
+const viewSpan = document.querySelector('#vSpan');
+const historySpan = document.querySelector('#hSpan');
+const storedNum = null;
+const tempArr = [];
+// either store individual numbers and operators or store together.
+
 const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
@@ -8,6 +15,49 @@ const divide = (a, b) => a / b;
 
 const operate = (operator, a, b) => operator(a, b);
 
-const numDisplay = () => {
-
+const numDisplay = (b) => {
+  console.log(this)
 }
+
+// add eventlistners to all buttons
+
+buttons.forEach(button => {
+  if (button.classList.contains("numbers")) {
+    button.addEventListener('click', function() {
+      viewSpan.innerHTML += this.innerHTML;
+      tempArr.push(this.innerHTML);
+      console.log(tempArr);
+    });
+  } else if (button.dataset.key === '27') {
+    button.addEventListener('click', function() {
+      viewSpan.innerHTML = '';
+      historySpan.innerHTML = '';
+      tempArr.length = 0;
+      console.log(tempArr)
+    });
+  } else if (button.dataset.key === '8') {
+    button.addEventListener('click', function() {
+      if (viewSpan.innerHTML !== '') {
+        tempArr.pop();
+        console.log(tempArr)
+        viewSpan.innerHTML = tempArr;
+      } else {
+        console.log(`nothing to delete`)
+        return;
+      }
+      // if (!viewSpan.innerHTML === '') {
+      //   return console.log(`viewspan is active`);
+      // } else {
+      //   return console.log(`nothing here`);
+      // }
+    })
+  } else {
+    return;
+  }
+  // button.addEventListener('keydown', )
+})
+
+
+/*
+Create the functions that populate the display when you click the number buttons… you should be storing the ‘display value’ in a variable somewhere for use in the next step.
+*/
