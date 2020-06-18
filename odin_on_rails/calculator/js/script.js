02087a7c16a9272afe69e6d38c22272cd4428964
THREEhundo <samuelbaik87@gmail.com>
2020-06-17 23:32:50 -0400

    Fixed chaining operations bug. Fixed history display bug.

--- odin_on_rails/calculator/js/script.js
+++ odin_on_rails/calculator/js/script.js
@@ -1,8 +1,10 @@
 const buttons = document.querySelectorAll('button');
 const viewSpan = document.querySelector('#vSpan');
 const historySpan = document.querySelector('#hSpan');
-const storedNum = null;
-const tempArr = [];
+const first = [];
+const second = [];
+const operation = [];
+let answer;
 // either store individual numbers and operators or store together.
 
 const add = (a, b) => a + b;
@@ -23,56 +25,106 @@ const numDisplay = (b) => {
 
 buttons.forEach(button => {
   if (button.classList.contains("numbers")) {
+    // Numbers
     button.addEventListener('click', function() {
       viewSpan.innerHTML += this.innerHTML;
-      tempArr.push(this.innerHTML);
+      if (operation.length > 0) {
+        second.push(this.innerHTML);
+        console.log(first, operation, second);
+      } else {
+        first.push(this.innerHTML);
+        console.log(first, operation, second);
+      }
     });
   } else if (button.dataset.key === '27') {
+    // Clear
     button.addEventListener('click', function() {
       viewSpan.innerHTML = '';
       historySpan.innerHTML = '';
-      tempArr.length = 0;
+      first.length = 0;
+      second.length = 0;
+      operation.length = 0;
     });
   } else if (button.dataset.key === '8') {
+    // Delete
     button.addEventListener('click', function() {
       if (viewSpan.innerHTML !== '') {
-        tempArr.pop();
-        viewSpan.innerHTML = tempArr;
+        if (second.length > 0) {
+          second.pop();
+        } else if (operation.length > 0) {
+          operation.pop();
+        } else {
+          first.pop();
+          viewSpan.innerHTML = first.join('');
+          console.log(first);
+        }
       } else {
         return;
       }
     })
   } else if (button.classList.contains('operator')) {
+    // Operators
     button.addEventListener('click', function() {
+      if (operation.length > 0) {
+
+      }
+      if (second.length > 0) {
+        second.reduce((acc, curr) => acc += curr);
+      }
+      first.reduce((acc, curr) => acc += curr);
       viewSpan.innerHTML += ` ${button.innerHTML} `;
-      tempArr.push(button.innerHTML);
+      operation.push(button.innerHTML);
+      console.log(first, operation, second);
     });
   } else if (button.dataset.key === '13') {
+    // Equals
     button.addEventListener('click', function() {
-      // tempArr should be shown in historySpan
-
-      const history = tempArr.join(' ');
-      historySpan.innerHTML += history;
+      // push 2nd argument to array
+      // arrays should be shown in historySpan
+      historySpan.innerHTML = '';
+      historySpan.innerHTML = viewSpan.innerHTML;
+      // return single value if second array is empty.
+      if (second.length === 0) {
+        return
+      }
       // operation function runs
-      const operator = tempArr[1];
-      const firstArg = parseInt(tempArr[0]);
-      const secondArg = parseInt(tempArr[2]);
-      console.log(tempArr);
-      switch (operator) {
+      const firstArg = parseInt(first.reduce((acc, curr) => acc += curr));
+      const secondArg = parseInt(second.reduce((acc, curr) => acc += curr));
+      // let answer;
+      switch (operation[0]) {
         case '÷':
-          viewSpan.innerHTML = operate(divide, firstArg, secondArg);
+          answer = operate(divide, firstArg, secondArg);
+          viewSpan.innerHTML = answer;
+          first.length = 0;
+          first.push(answer);
+          second.length = 0;
+          operation.length = 0;
           break;
         case 'x':
-          viewSpan.innerHTML = operate(multiply, firstArg, secondArg);
+          answer = operate(multiply, firstArg, secondArg);
+          viewSpan.innerHTML = answer;
+          first.length = 0;
+          first.push(answer);
+          second.length = 0;
+          operation.length = 0;
           break;
         case '+':
           console.log(operate(add, firstArg, secondArg));
-          viewSpan.innerHTML = operate(add, firstArg, secondArg);
+          answer = operate(add, firstArg, secondArg);
+          viewSpan.innerHTML = answer;
+          first.length = 0;
+          first.push(answer);
+          second.length = 0;
+          operation.length = 0;
           break;
         case '-':
-          viewSpan.innerHTML = operate(subtract, firstArg, secondArg);
+          answer = operate(subtract, firstArg, secondArg);
+          viewSpan.innerHTML = answer;
+          first.length = 0;
+          first.push(answer);
+          second.length = 0;
+          operation.length = 0;
           break;
-
       }
 
       // operation output shown in viewSpan
