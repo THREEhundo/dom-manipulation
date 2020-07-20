4d3af3f15f82ffa582cbd6c505cfb630228d7f1b
THREEhundo <samuelbaik87@gmail.com>
2020-07-20 16:01:34 -0400

    Changed gameboard into a private variable. Converted squares Nodelist into array to grab index of each square.

--- Tic-Tac-Toe/README.md
+++ Tic-Tac-Toe/README.md
@@ -3,3 +3,19 @@
 # Functionality
 
 # Process
+Figure out which method and variables fit in each module or factory function.
+What remains hidden & what remains visible?
+
+Player should keep track of score & moves.
+Game board should keep track of the moves being placed on the board.
+Game flow should control the methods that go between Player factory functions and game board module.
+
+Problem - X's & O's
+Players input either X or O values using splice.
+Game board makes use of Player splice method?
+Game flow places X's & O's on DOM.
+Game board -> Player -> Game flow
+
+Changed board.gameboard into a private variable
+
+Changed squares Nodelist into an array to grab index of each node.
--- Tic-Tac-Toe/js/app.js
+++ Tic-Tac-Toe/js/app.js
@@ -2,41 +2,56 @@
 
 // Game board
 const board = (() => {
-  const gameboard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+  const gameboard = ['', '', '', '', '', '', '', '', ''];
 
   function _empty(arr) {
     arr === undefined;
   }
-
-  // function _switchText() {
-  //   if (_gameboard.every(_empty)) {
-  //     return 'X';
-  //   } else if () {
-  //
-  //   }
-  // }
-
 
   const squares = document.querySelectorAll('.square');
   squares.forEach((square) => {
     // square.addEventListener('click', _pushPlayerChar.bind(square, square, 'X'));
   });
 
+  function hiddenBoard() {
+    return gameboard;
+  }
 
   return {
-    gameboard
+    hiddenBoard
   };
 })();
 
 // Game Flow
 const gameFlowController = (() => {
 
-  function pushGameboard(elem, index, array) {
-    //elem.innerText = board.gameboard[index];
+  function _empty(arr) {
+    arr === undefined;
   }
 
+  const boardArray = board.hiddenBoard();
+
+  function _pushGameboard(elem, index, array) {
+    //elem.innerText = board.gameboard[index];
+    // How do I choose player1 (X) or player2 (O)
+    // player1.boardSplice() || player2.boardSplice()
+
+    // if (_empty(array) && player1.score === 0 && player2.score === 0) {
+
+    // player1.boardSplice(index, 'X');
+    // elem.innerText = boardArray[index];
+    console.log(elem, index, array)
+    // }
+  }
+
+  // Score has to be 0 - 0 && array === undefined
   const squares = document.querySelectorAll('.square');
-  squares.forEach(pushGameboard);
+
+  const squaresArr = [...squares];
+
+  squaresArr.forEach(square => {
+    square.addEventListener('click', _pushGameboard.bind(square, square, squaresArr.indexOf(square), boardArray))
+  });
 
 
 
@@ -48,26 +63,26 @@ const gameFlowController = (() => {
 const Player = (piece) => {
   let score = 0;
   const {
-    gameboard
+    hiddenBoard
   } = board;
 
   // 7 Winning conditions
-  if ((gameboard[0] && gameboard[1] && gameboard[2] === 'piece') ||
-    (gameboard[3] && gameboard[4] && gameboard[5] === 'piece') ||
-    (gameboard[6] && gameboard[7] && gameboard[8] === 'piece') ||
-    (gameboard[0] && gameboard[4] && gameboard[8] === 'piece') ||
-    (gameboard[2] && gameboard[4] && gameboard[6] === 'piece') ||
-    (gameboard[0] && gameboard[3] && gameboard[6] === 'piece') ||
-    (gameboard[1] && gameboard[4] && gameboard[7] === 'piece') ||
-    (gameboard[2] && gameboard[5] && gameboard[8] === 'piece')) {
+  if ((hiddenBoard[0] && hiddenBoard[1] && hiddenBoard[2] === 'piece') ||
+    (hiddenBoard[3] && hiddenBoard[4] && hiddenBoard[5] === 'piece') ||
+    (hiddenBoard[6] && hiddenBoard[7] && hiddenBoard[8] === 'piece') ||
+    (hiddenBoard[0] && hiddenBoard[4] && hiddenBoard[8] === 'piece') ||
+    (hiddenBoard[2] && hiddenBoard[4] && hiddenBoard[6] === 'piece') ||
+    (hiddenBoard[0] && hiddenBoard[3] && hiddenBoard[6] === 'piece') ||
+    (hiddenBoard[1] && hiddenBoard[4] && hiddenBoard[7] === 'piece') ||
+    (hiddenBoard[2] && hiddenBoard[5] && hiddenBoard[8] === 'piece')) {
     // Modal function pops
     // Player wins!
     score++;
   }
 
   // Player can splice element in board array
-  function boardSplice(pos, piece) {
-    b.splice(gameboard[pos], 1, piece);
+  function boardSplice(index, piece) {
+    gameboard.splice(hiddenBoard[index], 1, piece);
     console.log(piece);
   }
 
