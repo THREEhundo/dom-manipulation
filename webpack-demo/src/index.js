2398d4eb25b5a5eb9fc976369974a66a24191199
THREEhundo <samuelbaik87@gmail.com>
2020-09-02 00:49:16 -0400

    Used both default export and named export modules.

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,37 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+//
+// function component() {
+//   const element = document.createElement('div');
+//
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+import myName from './myName';
 
 function component() {
-  const element = document.createElement('div');
+  var element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.innerHTML = myName('Sam');
   return element;
 }
 
 document.body.appendChild(component());
+
+import {
+  functionOne,
+  functionTwo
+} from './myModule'
+
+function functs() {
+  var element = document.createElement('div');
+
+  element.innerHTML = `${functionOne()}, ${functionTwo()}, THREE, FOUR, FIVE`;
+  return element;
+}
+
+document.body.appendChild(functs());
