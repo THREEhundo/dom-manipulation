0d5c44fd6d72adabd6fb76c80a5322d6772840aa
THREEhundo <samuelbaik87@gmail.com>
2020-09-08 21:45:25 -0400

    added animation for tab titles

--- odin_on_rails/Restaurant/src/home.js
+++ odin_on_rails/Restaurant/src/home.js
@@ -1,5 +1,6 @@
 const home = () => {
   const content = document.querySelector('#content');
+  const pages = ['home', 'about', 'menu', 'contact'];
 
   const header = document.createElement('h1');
   header.id = 'bannerHeader'
@@ -13,26 +14,42 @@ const home = () => {
   content.appendChild(bImg);
 
   // tabs for pages
-  const pages = ['home', 'about', 'menu', 'contact'];
   const tabsContainer = document.createElement('div');
   tabsContainer.id = 'tabsContainer';
-  pages.forEach((page) => {
+
+  const tabsTitleContainer = document.createElement('div');
+  tabsTitleContainer.id = 'titleContainer';
+
+
+  pages.forEach((page, index) => {
+    const title = document.createElement('div');
+    title.innerHTML = page;
+    title.classList.add('tabTitle', 'tab');
+    title.id = `title${index}`;
+    tabsTitleContainer.appendChild(title);
+
     const tab = document.createElement('div');
     tab.classList.add('tab');
-    tab.id = page;
+    tab.classList.add(page);
     tabsContainer.appendChild(tab);
     tab.addEventListener('mouseover', (e) => {
       if (tab.classList.contains('scale-down-ver-bottom')) {
         tab.classList.remove('scale-down-ver-bottom');
       }
-      tab.classList.add('scale-up-ver-bottom')
+      tab.classList.add('scale-up-ver-bottom');
+      if (title.classList.contains('tracking-in-expand-fwd-bottom')) {
+        title.classList.remove('tracking-in-expand-fwd-bottom');
+      }
+      title.classList.add('tracking-in-expand-fwd-bottom');
     });
     tab.addEventListener('mouseout', (e) => {
       tab.classList.add('scale-down-ver-bottom');
       tab.classList.remove('scale-up-ver-bottom');
+      // title.classList.add('tracking-in-expand-fwd-top');
+      // title.classList.remove('tracking-in-expand-fwd-bottom');
     });
   });
-
+  content.appendChild(tabsTitleContainer);
   content.appendChild(tabsContainer);
 
 }
