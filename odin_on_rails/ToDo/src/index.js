02166a72bef89e78bcd7afa6ef0cd51c672f00d3
THREEhundo <samuelbaik87@gmail.com>
2020-09-22 20:41:27 -0400

    added ToDoItem module

--- odin_on_rails/ToDo/src/index.js
+++ odin_on_rails/ToDo/src/index.js
@@ -1,1 +1,22 @@
 console.log(`CURAHEE`);
+
+const lists = {
+  gym: {
+    title: "Pull-up bar",
+    description: "Research pull-up bar prices",
+    date: 12 / 12 / 21,
+    priority: "low",
+    notes: "text-box",
+    checklist: {
+      1: "www.roguefitness.com",
+      2: "www.adidas.com",
+      3: "www.nike.com"
+    }
+  },
+  shopping: {
+
+  },
+  work: {
+
+  }
+}
