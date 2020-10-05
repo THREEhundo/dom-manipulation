371dc8c56e68db1d67e06ddad4566c86dc5f64f8
Samuel Baik <Hundo@samuels-mbp.home>
2020-10-05 13:02:17 -0700

    Added modules to index.js Added sidebar module

--- odin_on_rails/ToDo/src/index.js
+++ odin_on_rails/ToDo/src/index.js
@@ -1,53 +1,7 @@
-console.log(`CURAHEE`);
+import ToDoItem from './todoItem';
+import Lists from './todoList';
+import Sidebar from './sidebar';
 
-// const lists = {
-//   gym: {
-//     title: "Pull-up bar",
-//     description: "Research pull-up bar prices",
-//     date: 12 / 12 / 21,
-//     priority: "low",
-//     notes: "text-box",
-//     checklist: {
-//       1: "www.roguefitness.com",
-//       2: "www.adidas.com",
-//       3: "www.nike.com"
-//     }
-//   },
-//   shopping: {
-//
-//   },
-//   work: {
-//
-//   }
-// }
-//
-// const shoes = {
-//   nike: {
-//     running: 'air-max',
-//     colors: {
-//       1: 'blue',
-//       2: 'red',
-//       3: 'green'
-//     }
-//   },
-//   adidas: {
-//     running: 'boost',
-//     colors: {
-//       1: 'blue',
-//       2: 'red',
-//       3: 'green'
-//     }
-//   }
-// }
-// for (let shoe in shoes) {
-//   for (let desc in shoes[shoe]) {
-//     if (desc == 'colors') {
-//       // console.log(shoes[shoe][desc]);
-//       for (let color in desc) {
-//         console.log(shoes[shoe][desc][color]);
-//       }
-//     }
-//     // console.log(desc);
-//     // console.log(shoes[shoe][desc]);
-//   }
-// }
+ToDoItem();
+Lists();
+Sidebar();
---
+++ odin_on_rails/ToDo/src/sidebar.js
@@ -0,0 +1,22 @@
+const Sidebar = () => {
+  const createTask = document.querySelector('#create-task');
+  const allTasks = document.querySelector('#all-tasks');
+  const lists = document.querySelector('#lists');
+  const calendar = document.querySelector('#calendar');
+
+  // Create Task Modal
+  const taskModal = () => {
+    const popUp = document.createElement('div');
+    popUp.id = 'task-modal';
+  }
+
+  createTask.addEventListener('click', )
+
+  return {
+
+  }
+}
+
+export {
+  Sidebar
+}
