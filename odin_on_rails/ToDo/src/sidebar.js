0d159c8b8a6d8bb526c6679c980c3840a6f80fa7
Samuel Baik <Hundo@samuels-mbp.home>
2020-10-06 15:38:08 -0700

    fixed order of operations for the for loop in modalBuild

--- odin_on_rails/ToDo/src/sidebar.js
+++ odin_on_rails/ToDo/src/sidebar.js
@@ -5,40 +5,60 @@ const Sidebar = () => {
   const lists = document.querySelector('#lists');
   const calendar = document.querySelector('#calendar');
   const taskModal = document.querySelector('#task-modal');
+  // const createTaskNotes = document.querySelector('#create-task-notes');
 
   const modalFeatures = {
     "item-container-0" :{
+      type: 'input',
       for: 'list-name',
       innerHTML: 'Task: ',
-      id: 'list-name',
-      required: true
+      required: true,
     },
-    // "item-container-1": {
-    //
-    // }
+    "item-container-1": {
+      type: 'input',
+      for: 'description',
+      innerHTML: 'Description: ',
+    },
+    "item-container-2": {
+      type: 'textarea',
+      for: 'notes',
+      innerHTML: 'Notes: ',
+      rows: 5,
+      cols: 30
+    }
+
   }
 
 function modalBuild(obj, form) {
+  let popupTitleInput;
+  let notes;
   for (let item in obj) {
     const itemContainer = document.createElement('div');
     itemContainer.classList.add('popup-item-container');
 
     const popupTitleLabel = document.createElement('label');
-    const popupTitleInput = document.createElement('input');
 
     for (let [key, value] of Object.entries(obj[item])) {
-      if (key == 'for') {
+      if (value == 'input') {
+        popupTitleInput = document.createElement('input');
+      } else if (key == 'for') {
         popupTitleLabel.setAttribute("for", value);
+        popupTitleInput.id = value;
       } else if (key == 'innerHTML') {
         popupTitleLabel.innerHTML = value;
-      } else if (key == 'id') {
-        popupTitleInput.id = value;
+      } else if (key == 'required') {
         popupTitleInput.required = value;
+      } else if (key == 'rows') {
+        notes = document.createElement('textarea');
+        notes.id = 'create-task-notes';
+        notes.setAttribute('rows', value);
+        notes.setAttribute('cols', value);
       }
     }
     form.appendChild(itemContainer);
     itemContainer.appendChild(popupTitleLabel);
     itemContainer.appendChild(popupTitleInput);
+    // itemContainer.appendChild(notes);
   }
 }
 
