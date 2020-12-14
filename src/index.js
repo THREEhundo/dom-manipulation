d1ee7c3d5f27ca932351dc347a63ba01717eb648
threeHUNDO <samuelbaik87@gmail.com>
2020-12-14 15:00:18 -0800

    react notes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Clock from "./components/Clock";
+// import Home from "./components/Home";
+// import Header from "./components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Clock />
+    <Clock />
+    <Clock />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
